499b50cc92dfd0cfc821a7fb7317a2450da74e2a
rladbsgus94@gmail.com <rladbsgus94@gmail.com>
2019-08-15 20:15:47 +0900

    MapsActivity 현재 위치 표시

--- app/src/main/java/com/e/codingmon/MainActivity.java
+++ app/src/main/java/com/e/codingmon/MainActivity.java
@@ -1,7 +1,8 @@
 package com.e.codingmon;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
