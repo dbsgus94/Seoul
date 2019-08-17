cfe3f5847a9cbf261c97b9fa1d3e1d0bbf262502
hsw <poohpama@naver.com>
2019-08-17 16:26:20 +0900

    일단 커밋은 하는데 해결된거 없음

--- app/src/main/java/com/e/codingmon/MainActivity.java
+++ app/src/main/java/com/e/codingmon/MainActivity.java
@@ -1,14 +1,24 @@
 package com.e.codingmon;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override //SplashActivity
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
