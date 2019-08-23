c2b056d1719a04d655d698183eaa4c6aca77be40
rladbsgus94@gmail.com <rladbsgus94@gmail.com>
2019-08-23 16:01:37 +0900

    지원이 main

--- app/src/main/java/com/e/codingmon/MainActivity.java
+++ app/src/main/java/com/e/codingmon/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Gallery;
+import android.widget.GridView;
 import android.widget.ImageView;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
