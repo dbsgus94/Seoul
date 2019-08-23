b70f0668fa78090289cf1b6aa594cf37b491faea
hsw <poohpama@naver.com>
2019-08-23 13:56:26 +0900

    animation 추가 / HomeActivity 이미지 추가 / WelcomeActivity 이미지 추가 / 코딩몬 화이팅

--- app/src/main/java/com/e/codingmon/WelcomeActivity.java
+++ app/src/main/java/com/e/codingmon/WelcomeActivity.java
@@ -6,25 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class HomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_welcome);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent welcomeIntent = new Intent(HomeActivity.this, MapsActivity.class);
-                startActivity(welcomeIntent);
+                Intent homeIntent = new Intent(WelcomeActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
                 finish();
 
-                startActivity(new Intent(HomeActivity.this, MapsActivity.class));
+                startActivity(new Intent(WelcomeActivity.this, HomeActivity.class));
                 overridePendingTransition(R.anim.anim_fade_in, R.anim.anim_fade_out);
-
+                /*
+                startActivity(new Intent(WelcomeActivity.this, HomeActivity.class));
+                overridePendingTransition(R.anim.anim_left_out, R.anim.anim_right_in);
+                finish();
+                */
             }
         }, SPLASH_TIME_OUT);
     }
 }
+
