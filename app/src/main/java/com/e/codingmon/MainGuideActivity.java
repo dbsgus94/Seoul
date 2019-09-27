df7a58ed479b2d44c2902d89cf087c7ee73da751
HSW_1005 <poohpama@gmail.com>
2019-09-27 20:13:31 +0900

    App Launcher Icon

--- app/src/main/java/com/e/codingmon/MainGuideActivity.java
+++ app/src/main/java/com/e/codingmon/MainGuideActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class MainGuideActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 1500;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
